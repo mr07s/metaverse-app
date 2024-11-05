496d660789f12d747e3d3a0ef6ebf61e63a12a82
mr07s <106861707+mr07s@users.noreply.github.com>
2024-11-05 14:18:43 +0530

    User endpoint added

--- my-turborepo/apps/http_backend/src/routes/v1/user.ts
+++ my-turborepo/apps/http_backend/src/routes/v1/user.ts
@@ -3,19 +3,64 @@ import { UpdateMetaDataSchema } from "../../types";
 import client from "@repo/db/client";
 import { userMiddleware } from "../../middleware/user";
 export const userRouter = Router();
+import { Request, Response } from "express";
 
 //updating meta data
-userRouter.post("/metadata", userMiddleware, (req, res) => {
-  const parsedData = UpdateMetaDataSchema.safeParse(req.body);
-  if (!parsedData?.success) {
-    res.status(400).json({ message: "Validation failed" });
-    return;
+userRouter.post(
+  "/metadata",
+  userMiddleware,
+  async (req: Request, res: Response) => {
+    const parsedData = UpdateMetaDataSchema.safeParse(req.body);
+    if (!parsedData?.success) {
+      res.status(400).json({ message: "Validation failed" });
+      return;
+    }
+    try {
+      await client.user.update({
+        where: { id: req?.userId },
+        data: {
+          avatarId: parsedData.data.avatarId,
+        },
+      });
+      res.status(200).json({ message: "Meta data updated" });
+      return;
+    } catch (e) {
+      res.status(400).json({ message: "Failed to update" });
+      return;
+    }
   }
-  //   client.user.update({
-  //     where: {},
-  //   });
-});
+);
 
 //get others metadeta
 
-userRouter.get("/metadata/bulk", (req, res) => {});
+userRouter.get("/metadata/bulk", userMiddleware, async (req, res) => {
+  try {
+    const userIdString = (req.query.ids ?? "[]") as string;
+    console.log(userIdString);
+    const userIds = userIdString.slice(1, userIdString?.length - 2).split(",");
+    // console.log(typeof userIds);
+    const metadata = await client.user.findMany({
+      where: {
+        id: {
+          in: userIds,
+        },
+      },
+      select: {
+        avatar: true,
+        id: true,
+      },
+    });
+    res.status(200).json({
+      avatars: metadata.map((m) => ({
+        userId: m.id,
+        imageUrl: m.avatar?.imageUrl,
+      })),
+    });
+    return;
+  } catch (e) {
+    res.status(400).json({
+      message: "Geeting metadata failed",
+    });
+    return;
+  }
+});
